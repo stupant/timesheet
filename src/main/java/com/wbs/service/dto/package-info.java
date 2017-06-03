67067c798de1f97e5d83cde2fbdcae2514154d88
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2017-06-03 22:42:22 +0700

    Initial import

---
+++ src/main/java/com/wbs/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.wbs.service.dto;
---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,53 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    TimesheetSharedLibsModule,
+    TimesheetSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        TimesheetSharedLibsModule,
+        TimesheetSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        TimesheetSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class TimesheetSharedModule {}
